1969d93f02e918a3438083b29f248d0341c6c607
Brendan Grubb and Erica Wright <erica@>
2017-02-16 10:13:50 -0800

    update arrays to remove 0 and update array search

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -3,30 +3,30 @@
     {
         function findScore($input)
         {
-            $one_point = array("0", "a", "e", "i", "o", "u", "l", "n", "r", "s", "t");
-            $two_point = array("0", "d", "g");
-            $three_point = array("0", "b", "c", "m", "p");
-            $four_point = array("0", "f", "h", "v", "w", "y");
-            $five_point = array("0", "k");
-            $eight_point = array("0", "j", "x");
-            $ten_point = array("0", "q", "z");
+            $one_point = array("a", "e", "i", "o", "u", "l", "n", "r", "s", "t");
+            $two_point = array("d", "g");
+            $three_point = array("b", "c", "m", "p");
+            $four_point = array("f", "h", "v", "w", "y");
+            $five_point = array("k");
+            $eight_point = array("j", "x");
+            $ten_point = array("q", "z");
             $word_letter = str_split($input);
             $total_points = 0;
 
             foreach ($word_letter as $letter) {
-                if (array_search($letter, $one_point) > 0) {
+                if (array_search($letter, $one_point) > 0 || array_search($letter, $one_point) === 0) {
                     ++ $total_points;
-                } else if (array_search($letter, $two_point) > 0) {
+                } else if (array_search($letter, $two_point) > 0 || array_search($letter, $two_point) === 0) {
                     $total_points = $total_points + 2;
-                } else if (array_search($letter, $three_point) > 0) {
+                } else if (array_search($letter, $three_point) > 0 || array_search($letter, $three_point) === 0) {
                     $total_points = $total_points + 3;
-                } else if (array_search($letter, $four_point) > 0) {
+                } else if (array_search($letter, $four_point) > 0 || array_search($letter, $four_point) === 0) {
                     $total_points = $total_points + 4;
-                } else if (array_search($letter, $five_point) > 0) {
+                } else if (array_search($letter, $five_point) > 0 || array_search($letter, $five_point) === 0) {
                     $total_points = $total_points + 5;
-                } else if (array_search($letter, $eight_point) > 0) {
+                } else if (array_search($letter, $eight_point) > 0 || array_search($letter, $eight_point) === 0) {
                     $total_points = $total_points + 8;
-                } else if (array_search($letter, $ten_point) > 0) {
+                } else if (array_search($letter, $ten_point) > 0 || array_search($letter, $ten_point) === 0) {
                     $total_points = $total_points + 10;
                 }
             }
